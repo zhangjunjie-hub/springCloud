9df999fa0a3b64398e992ef06d4cd9c1fcd2be70
zhangjunjie-hub <960057926@qq.com>
2021-04-14 16:43:10 +0800

    增加hystrix的熔断机制，增加redis的相关配置类和配置参数以及redisPool的相关的工具方法

--- springcloud/springcloud-parent/springcloud-eureka-apiinfo/src/main/java/com/springcloud/springcloudeurekaapiinfo/service/StudentService.java
+++ springcloud/springcloud-parent/springcloud-eureka-apiinfo/src/main/java/com/springcloud/springcloudeurekaapiinfo/service/StudentService.java
@@ -1,7 +1,10 @@
 package com.springcloud.springcloudeurekaapiinfo.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.springcloud.springcloudeurekaapiinfo.dto.Student;
 import com.springcloud.springcloudeurekaapiinfo.mapper.StudentMapper;
+import com.springcloud.springcloudeurekaapiinfo.utils.RedisUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,8 @@ import java.util.List;
 public class StudentService {
     @Autowired
     private StudentMapper studentMapper;
+    @Autowired
+    private RedisUtils redisUtils;
 
     /**
      * 添加学生信息
@@ -22,7 +27,17 @@ public class StudentService {
 
 
     public List<Student> getAllStudents() {
-
-        return studentMapper.getAllStudents();
+        List<Student> allStudents = null ;
+        //这里先从redis缓存中获取，
+        String allStudentsStr = redisUtils.get("all_students");
+        if(StringUtils.isBlank(allStudentsStr)){
+            //如果缓存为空，则进行数据库查询，并将查询到的数据存入到redis中
+            allStudents = studentMapper.getAllStudents();
+            redisUtils.set("all_students", JSONObject.toJSONString(allStudents),0);
+        }else{
+            //如果查询到的数据是存在的，则将数据转化为list
+            allStudents = JSONObject.parseArray(allStudentsStr, Student.class);
+        }
+        return allStudents;
     }
 }
---
+++ springcloud/springcloud-parent/springcloud-eureka-apiinfo/src/main/java/com/springcloud/springcloudeurekaapiinfo/utils/SystemConstant.java
@@ -0,0 +1,17 @@
+package com.springcloud.springcloudeurekaapiinfo.utils;
+
+/**
+ * @author  zhangjunjie
+ * 这个是用来放置系统常量
+ */
+public class SystemConstant {
+    public static final Integer SYSTEM_NUN_0 = 0;
+    public static final Integer SYSTEM_NUN_2 = 2;
+
+
+
+
+
+
+
+}
