a609dd84b4c63424b03ce29d876b980cac43ce98
zhangjunjie-hub <960057926@qq.com>
2021-05-01 09:53:48 +0800

    增加zuul网关配置，将mysql迁移至远程云地址

--- springcloud/springcloud-parent/springcloud_zuul/src/main/java/com/java/springboot/zuul/ZuulApplication.java
+++ springcloud/springcloud-parent/springcloud_zuul/src/main/java/com/java/springboot/zuul/ZuulApplication.java
@@ -3,7 +3,7 @@ package com.java.springboot.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -12,10 +12,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class ZuulApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ZuulApplication.class);
+        SpringApplication.run(ZuulApplication.class,args);
     }
 }
